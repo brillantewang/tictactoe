13e8ac3b8a54c3283d377fc616e3776481ae1f09
Brillante Wang <brillante.wang@gmail.com>
2018-04-13 17:34:28 -0700

    create boardContainer and wrap App component with Provider

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Board from './board'
+import Board from './boardContainer';
 
 class App extends Component {
   render() {
